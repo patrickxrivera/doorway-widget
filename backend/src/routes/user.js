d34b613d8d86b2ce234b9e1caf24e906d68303bf
pxr13 <patrick.x.rivera@gmail.com>
2020-10-20 18:00:46 -0500

    Add Sentry for error handling on the backend

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -2,13 +2,8 @@ const User = require("../services/user");
 
 const setupUserRoutes = (server) => {
     server.post("/user/create", async (req, res) => {
-        try {
-            const user = await User.create(req.body.email);
-            res.json({ success: true, userId: user.id });
-        } catch (e) {
-            res.status(500);
-            res.json({ success: false, errorMessage: e.message });
-        }
+        const user = await User.create(req.body.email);
+        res.json({ success: true, userId: user.id });
     });
 }
 
