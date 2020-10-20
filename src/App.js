956d250decdaf31fa214dd52a3e3f1afe2f8de1f
pxr13 <patrick.x.rivera@gmail.com>
2020-10-20 12:09:41 -0500

    Add modal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const handleGetAccessPress = () => {
-    console.log("pressed get access!")
-  }
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
     <div className="follow-gate-widget__container">
-      <Button onClick={handleGetAccessPress} variant="primary">Get Access</Button>
+      <Button onClick={handleShow} variant="secondary">Get Access</Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Welcome to Patrick's Follow Gate 👋</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Follow me on Twitter and give me your email... or else 🗡️</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Continue
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
