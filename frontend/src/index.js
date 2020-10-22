64854c2e2627e6af9a44c9c63e8c008b403bd388
pxr13 <patrick.x.rivera@gmail.com>
2020-10-21 20:45:59 -0500

    Reference follow-gate-widget element

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,10 @@
+import 'regenerator-runtime/runtime';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { setupSentry } from './setup/sentry';
-import 'regenerator-runtime/runtime';
 
 setupSentry();
 
@@ -11,5 +12,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('follow-gate-widget')
 );
