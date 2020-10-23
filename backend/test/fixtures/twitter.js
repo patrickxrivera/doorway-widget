3876de1703d97e0ff603f8d0f86100c695aa6005
pxr13 <patrick.x.rivera@gmail.com>
2020-10-22 20:51:43 -0500

    Basic tests for twitter service

---
+++ backend/test/fixtures/twitter.js
@@ -0,0 +1,26 @@
+const requestTokenResponse = {
+    "oauth_token": "nn-i5QAAAAABJAW9AAABdVJd9L8",
+    "oauth_token_secret": "KxFMiAkrWHuf6T5fxMftmFIwHLJvL9y5",
+    "oauth_callback_confirmed": "true"
+}
+
+const accessTokenResponse = {
+    "oauth_token": "nn-i5QAAAAABJAW9AAABdVJd9L8",
+    "oauth_token_secret": "KxFMiAkrWHuf6T5fxMftmFIwHLJvL9y5",
+    "user_id": 1,
+    "screen_name": "patrickxrivera"
+}
+
+const accessTokenArgs =     {
+    "oAuthToken": "JBfaOAAAAAABJAW9AAABdVJsxyM",
+    "oAuthVerifier": "r3eIHQoOpqfjAxCCG22NEn4lwwCdrEcO"
+}
+
+const callbackUrl = "https://example.com";
+
+module.exports = {
+    requestTokenResponse,
+    accessTokenArgs,
+    accessTokenResponse,
+    callbackUrl
+}
